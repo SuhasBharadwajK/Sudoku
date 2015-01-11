8f33578f12a271b66f066389836ada8f98a0e997
Suhas Bharadwaj <suhas.five@gmail.com>
2015-01-12 01:39:21 +0530

    Four of Many.
    Added four square menus and corresponding storyboards to make them
    appear when and vanish as required. Remaining animation work: Seventy
    Seven Squares.

--- Sudoku/Sudoku.Windows/MainPage.xaml.cs
+++ Sudoku/Sudoku.Windows/MainPage.xaml.cs
@@ -25,13 +25,42 @@ namespace Sudoku
         public MainPage()
         {
             this.InitializeComponent();
+            //((Windows.UI.Xaml.Controls.TextBlock)this.FindName("textBlock_Copy")).Opacity = 0;
         }
 
         private void BoxTapped(object sender, TappedRoutedEventArgs e)
         {
             string senderInfo = ((TextBlock)sender).Name;
-            ((Windows.UI.Xaml.Controls.TextBlock)this.FindName("num" + 1 + "x" + 1 + "_1x1")).Text = "\n25";
-            ww.Begin();
+            int p = Int32.Parse(senderInfo.Substring(3, 1));
+            int q = Int32.Parse(senderInfo.Substring(5, 1));
+            int r = Int32.Parse(senderInfo.Substring(7, 1));
+            int s = Int32.Parse(senderInfo.Substring(9, 1));
+            ((Windows.UI.Xaml.Controls.TextBlock)this.FindName("num" + 1 + "x" + 1 + "_1x1")).Text = "\n25";    
+            if (senderInfo.Substring(3, 1) == "1")
+            {
+                //((Windows.UI.Xaml.Controls.TextBlock)this.FindName("textBlock_Copy")).Opacity = 0;
+                //((Windows.UI.Xaml.Controls.TextBlock)this.FindName("textBlock_Copy")).Visibility = Visibility.Collapsed;
+                //((Windows.UI.Xaml.Controls.TextBlock)this.FindName("textBlock_Copy")).IsTapEnabled = false;
+                //Canvas.SetLeft(textBox, 500);
+                //expand_1x1_1x2.Begin();
+                expand_1x1_2x1.Begin();
+            }
+            else if (senderInfo.Substring(3, 1) == "2")
+            {
+                //((Windows.UI.Xaml.Controls.TextBlock)this.FindName("textBlock_Copy")).Opacity = 0;
+                //((Windows.UI.Xaml.Controls.TextBlock)this.FindName("textBlock_Copy")).Visibility = Visibility.Collapsed;
+                //((Windows.UI.Xaml.Controls.TextBlock)this.FindName("textBlock_Copy")).IsTapEnabled = false;
+                //Canvas.SetLeft(textBox, 500);
+                //expand_1x1_1x2_R.Begin();
+                expand_1x1_2x1_R.Begin();
+            }
+            //(
+            
+        }
+
+        private void textBlock_Copy_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            ((Windows.UI.Xaml.Controls.TextBox)this.FindName("textBox")).Text = "Suhas";
         }
     }
 }
